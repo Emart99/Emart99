32d4006187c6e93252edc09cc908a11f8265b44e
Ezequiel Martino <ezequielmartino@MacBook-Pro-de-Ezequiel.local>
2022-06-30 17:32:00 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Home } from './views/Home';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route exact path='/' element={<Home />} />
     </Routes>
 
   );
